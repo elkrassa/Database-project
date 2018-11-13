53ade88c2d44790df69e6bdf706350e326d77a93
Eleftheria Krassa <User@DESKTOP-P9C7RLD>
2018-11-13 23:35:34 +0200

    ex

---
+++ Create/src/DataBase/DataBase.java
@@ -0,0 +1,21 @@
+
+package DataBase;
+
+public class DataBase {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void choose() {
+        
+      System.out.println("Welcome to your Database, what would you like to do?");
+      System.out.println("1. Create a new Database.");
+      System.out.println("2. Add something on your Database.");
+      System.out.println("3. Change something from your Database.");
+      System.out.println("4. Remove something from your Database.");
+      
+    }
+    
+}
